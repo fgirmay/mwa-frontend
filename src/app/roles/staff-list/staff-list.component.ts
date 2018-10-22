aeddfc2e39d1ad58eb0e3d130298d2bac97d9bc5
fissehaye <fissehaye@gmail.com>
2018-10-22 18:12:25 -0500

    Implemented user status change

--- src/app/roles/staff-list/staff-list.component.ts
+++ src/app/roles/staff-list/staff-list.component.ts
@@ -10,4 +10,15 @@ export class StaffListComponent {
   @Input() staffs: Staff[];
   displayedColumns: string[] = ['firstName', 'lastName', 'email', 'userName', 'role', 'status'];
   constructor() {}
+
+  onChange(staff: Staff) {
+    console.log('Status changed, value of status was ', staff.status);
+    if (staff.status === 'Active') {
+      staff.status = 'Inactive';
+      console.log('Inactiving user');
+    } else {
+      staff.status = 'Active';
+      console.log('Activating user');
+    }
+  }
 }
